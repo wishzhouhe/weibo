f2e7b1fbc71ae8da8fcbe785c64466119f9e8d17
zh2003 <zh2003@muyingzhijia.com>
2016-11-15 18:00:32 +0800

    添加工具类

---
+++ SinaWeibo/SinaWeibo/Weibo-Bridge.h
@@ -0,0 +1,11 @@
+//
+//  Weibo-Bridge.h
+//  SinaWeibo
+//
+//  Created by myzj2004 on 2016/11/15.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+//桥接文件,专门用于传入oc 的头文件,一旦引入,swift 就可以正常使用(宏除外)
+
+#import "CZAdditions.h"
