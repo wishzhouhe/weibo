70341823b6c76d2b7a38a4d2e2f6e099dfdfa4b5
zh2003 <zh2003@muyingzhijia.com>
2016-11-15 17:27:47 +0800

    添加文件目录结构,

---
+++ SinaWeibo/SinaWeibo/Classes/View(视图和控制器)/Main/WBMainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WBMainViewController.swift
+//  SinaWeibo
+//
+//  Created by myzj2004 on 2016/11/15.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+import UIKit
+
+class WBMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SinaWeibo/SinaWeibo/Classes/View(视图和控制器)/Profile/WBProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBProfileViewController.swift
+//  SinaWeibo
+//
+//  Created by myzj2004 on 2016/11/15.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+import UIKit
+
+class WBProfileViewController: WBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
